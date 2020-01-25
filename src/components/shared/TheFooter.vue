f3362662861da1573273ba73b0a16469a4a17bd2
Rhonda <rtinkham0408@gmail.com>
2020-01-24 20:05:21 -0500

    first commit

---
+++ src/components/shared/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Meetuper</strong> by <a href="#">Rhonda Tinkham</a>. The source code is licensed.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+</style>
